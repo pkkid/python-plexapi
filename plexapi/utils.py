21f376051cf8e49e0936d40a33064d8c0ec9505e
Hellowlol <hellowlol1@gmail.com>
2016-12-16 01:17:02 +0100

    s

--- plexapi/utils.py
+++ plexapi/utils.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 PlexAPI Utils
+
+Attributes:
+    LIBRARY_TYPES (dict): Description
+    NA (TYPE): Description
+    SEARCHTYPES (TYPE): Description
 """
 import re
 from datetime import datetime
@@ -10,33 +15,55 @@ from threading import Thread
 from plexapi import log
 
 # Search Types - Plex uses these to filter specific media types when searching.
-SEARCHTYPES = {'movie':1, 'show':2, 'season':3, 'episode':4, 'artist':8, 'album':9, 'track':10}
+SEARCHTYPES = {'movie': 1, 'show': 2, 'season': 3,
+               'episode': 4, 'artist': 8, 'album': 9, 'track': 10}
 
-# Registry of library types we may come across when parsing XML. This allows us to
-# define a few helper functions to dynamically convery the XML into objects.
-# see buildItem() below for an example.
 LIBRARY_TYPES = {}
+
+
 def register_libtype(cls):
+    """Registry of library types we may come across when parsing XML.
+    This allows us to define a few helper functions to dynamically convery
+    the XML into objects. See buildItem() below for an example.
+    """
+
     LIBRARY_TYPES[cls.TYPE] = cls
     return cls
 
-
-# This used to be a simple variable equal to '__NA__'. However, there has been need to
-# compare NA against None in some use cases. This object allows the internals of PlexAPI 
-# to distinguish between unfetched values and fetched, but non-existent values.
-# (NA == None results to True; NA is None results to False)
 class _NA(object):
-    def __bool__(self): return False
-    def __eq__(self, other): return isinstance(other, _NA) or other in [None, '__NA__']
-    def __nonzero__(self): return False
-    def __repr__(self): return '__NA__'
+    """This used to be a simple variable equal to '__NA__'.
+    However, there has been need to compare NA against None in some use cases.
+    This object allows the internals of PlexAPI to distinguish between unfetched
+    values and fetched, but non-existent values.
+    (NA == None results to True; NA is None results to False)
+    """
+
+    def __bool__(self):
+        return False
+
+    def __eq__(self, other):
+        return isinstance(other, _NA) or other in [None, '__NA__']
+
+    def __nonzero__(self):
+        return False
+
+    def __repr__(self):
+        return '__NA__'
+
 NA = _NA()
 
 
-# Not all objects in the Plex listings return the complete list of elements for the object.
-# This object will allow you to assume each object is complete, and if the specified value
-# you request is None it will fetch the full object automatically and update itself.
+
 class PlexPartialObject(object):
+    """Not all objects in the Plex listings return the complete list of elements
+    for the object.This object will allow you to assume each object is complete,
+    and if the specified value you request is None it will fetch the full object
+    automatically and update itself.
+
+    Attributes:
+        initpath (TYPE): Description
+        server (TYPE): Description
+    """
 
     def __init__(self, data, initpath, server=None):
         self.server = server
@@ -49,7 +76,7 @@ class PlexPartialObject(object):
     def __repr__(self):
         clsname = self.__class__.__name__
         key = self.key.replace('/library/metadata/', '') if self.key else 'NA'
-        title = self.title.replace(' ','.')[0:20].encode('utf8')
+        title = self.title.replace(' ', '.')[0:20].encode('utf8')
         return '<%s:%s:%s>' % (clsname, key, title)
 
     def __getattr__(self, attr):
@@ -58,7 +85,7 @@ class PlexPartialObject(object):
         self.reload()
         return self.__dict__.get(attr, NA)
 
-    def __setattr__(self, attr, value):     
+    def __setattr__(self, attr, value):
         if value != NA or self.isFullObject():
             super(PlexPartialObject, self).__setattr__(attr, value)
 
@@ -72,17 +99,28 @@ class PlexPartialObject(object):
         return not self.isFullObject()
 
     def reload(self):
-        """ Reload the data for this object from PlexServer XML. """
+        """Reload the data for this object from PlexServer XML.
+        """
         data = self.server.query(self.key)
         self.initpath = self.key
         self._loadData(data[0])
 
 
-# This is a general place to store functions specific to media that is Playable. Things
-# were getting mixed up a bit when dealing with Shows, Season, Artists, Albums which
-# are all not playable.
+
 class Playable(object):
-    
+    """This is a general place to store functions specific to media that is Playable. Things
+    were getting mixed up a bit when dealing with Shows, Season, Artists, Albums which
+    are all not playable.
+
+    Attributes:
+        player (TYPE): Description
+        playlistItemID (TYPE): Description
+        sessionKey (TYPE): Description
+        transcodeSession (TYPE): Description
+        username (TYPE): Description
+        viewedAt (TYPE): Description
+    """
+
     def _loadData(self, data):
         # data for active sessions (/status/sessions)
         self.sessionKey = cast(int, data.attrib.get('sessionKey', NA))
@@ -93,10 +131,11 @@ class Playable(object):
         self.viewedAt = toDatetime(data.attrib.get('viewedAt', NA))
         # data for playlist items
         self.playlistItemID = cast(int, data.attrib.get('playlistItemID', NA))
-    
+
     def getStreamURL(self, **params):
         if self.TYPE not in ('movie', 'episode', 'track'):
-            raise Unsupported('Fetching stream URL for %s is unsupported.' % self.TYPE)
+            raise Unsupported(
+                'Fetching stream URL for %s is unsupported.' % self.TYPE)
         mvb = params.get('maxVideoBitrate')
         vr = params.get('videoResolution', '')
         params = {
@@ -106,23 +145,43 @@ class Playable(object):
             'protocol': params.get('protocol'),
             'mediaIndex': params.get('mediaIndex', 0),
             'X-Plex-Platform': params.get('platform', 'Chrome'),
-            'maxVideoBitrate': max(mvb,64) if mvb else None,
+            'maxVideoBitrate': max(mvb, 64) if mvb else None,
             'videoResolution': vr if re.match('^\d+x\d+$', vr) else None
         }
-        params = {k:v for k,v in params.items() if v is not None}  # remove None values
+        # remove None values
+        params = {k: v for k, v in params.items() if v is not None}
         streamtype = 'audio' if self.TYPE in ('track', 'album') else 'video'
         return self.server.url('/%s/:/transcode/universal/start.m3u8?%s' % (streamtype, urlencode(params)))
-    
+
     def iterParts(self):
+        """Yield parts
+        """
         for item in self.media:
             for part in item.parts:
                 yield part
-    
+
     def play(self, client):
+        """Start playback on a client."""
         client.playMedia(self)
 
 
 def buildItem(server, elem, initpath, bytag=False):
+    """Build classes used by the plexapi.
+
+       Args:
+            server (Plexserver): Server
+            elem (ElementThree):
+            initpath (string): init path of the url, used to determin
+                               if this is a full object.
+            bytag (bool): Dunno # TOO
+
+        Returns:
+            library type
+
+        Raises:
+            UnknownType
+
+    """
     libtype = elem.tag if bytag else elem.attrib.get('type')
     if libtype == 'photo' and elem.tag == 'Directory':
         libtype = 'photoalbum'
@@ -133,6 +192,7 @@ def buildItem(server, elem, initpath, bytag=False):
 
 
 def cast(func, value):
+    """Helper to change to the correct type"""
     if value not in [None, NA]:
         if func == bool:
             return bool(int(value))
@@ -146,6 +206,7 @@ def cast(func, value):
 
 
 def findKey(server, key):
+    """Finds and builds a object based on ratingKey."""
     path = '/library/metadata/{0}'.format(key)
     try:
         # Item seems to be the first sub element
@@ -156,6 +217,7 @@ def findKey(server, key):
 
 
 def findItem(server, path, title):
+    """Finds and builds a object based on title."""
     for elem in server.query(path):
         if elem.attrib.get('title').lower() == title.lower():
             return buildItem(server, elem, path)
@@ -163,6 +225,15 @@ def findItem(server, path, title):
 
 
 def findLocations(data, single=False):
+    """Extract the path from a location tag
+
+       Args:
+            data (elementthree):
+            single (bool): One or more locations
+
+        Returns:
+            list: of filepaths
+    """
     locations = []
     for elem in data:
         if elem.tag == 'Location':
@@ -170,13 +241,22 @@ def findLocations(data, single=False):
     if single:
         return locations[0] if locations else None
     return locations
-    
+
 
 def findPlayer(server, data):
+    """Find a player in a elementthee
+
+       Args:
+            data (elementthree):
+
+        Returns:
+            PlexClient or None
+    """
     elem = data.find('Player')
     if elem is not None:
         from plexapi.client import PlexClient
-        baseurl = 'http://%s:%s' % (elem.attrib.get('address'), elem.attrib.get('port'))
+        baseurl = 'http://%s:%s' % (elem.attrib.get('address'),
+                                    elem.attrib.get('port'))
         return PlexClient(baseurl, server=server, data=elem)
     return None
 
@@ -207,7 +287,7 @@ def findUsername(data):
 
 
 def isInt(string):
-    try: 
+    try:
         int(string)
         return True
     except ValueError:
@@ -215,45 +295,54 @@ def isInt(string):
 
 
 def joinArgs(args):
-    if not args: return ''
+    if not args:
+        return ''
     arglist = []
-    for key in sorted(args, key=lambda x:x.lower()):
+    for key in sorted(args, key=lambda x: x.lower()):
         value = str(args[key])
         arglist.append('%s=%s' % (key, quote(value)))
     return '?%s' % '&'.join(arglist)
 
 
 def listChoices(server, path):
-    return {c.attrib['title']:c.attrib['key'] for c in server.query(path)}
+    return {c.attrib['title']: c.attrib['key'] for c in server.query(path)}
 
 
 def listItems(server, path, libtype=None, watched=None, bytag=False):
     items = []
     for elem in server.query(path):
-        if libtype and elem.attrib.get('type') != libtype: continue
-        if watched is True and elem.attrib.get('viewCount', 0) == 0: continue
-        if watched is False and elem.attrib.get('viewCount', 0) >= 1: continue
+        if libtype and elem.attrib.get('type') != libtype:
+            continue
+        if watched is True and elem.attrib.get('viewCount', 0) == 0:
+            continue
+        if watched is False and elem.attrib.get('viewCount', 0) >= 1:
+            continue
         try:
             items.append(buildItem(server, elem, path, bytag))
         except UnknownType:
             pass
     return items
-    
+
 
 def rget(obj, attrstr, default=None, delim='.'):
     try:
         parts = attrstr.split(delim, 1)
         attr = parts[0]
         attrstr = parts[1] if len(parts) == 2 else None
-        if isinstance(obj, dict): value = obj[attr]
-        elif isinstance(obj, list): value = obj[int(attr)]
-        elif isinstance(obj, tuple): value = obj[int(attr)]
-        elif isinstance(obj, object): value = getattr(obj, attr)
-        if attrstr: return rget(value, attrstr, default, delim)
+        if isinstance(obj, dict):
+            value = obj[attr]
+        elif isinstance(obj, list):
+            value = obj[int(attr)]
+        elif isinstance(obj, tuple):
+            value = obj[int(attr)]
+        elif isinstance(obj, object):
+            value = getattr(obj, attr)
+        if attrstr:
+            return rget(value, attrstr, default, delim)
         return value
     except:
         return default
-    
+
 
 def searchType(libtype):
     libtype = str(libtype)
@@ -278,6 +367,8 @@ def threaded(callback, listargs):
 
 def toDatetime(value, format=None):
     if value and value != NA:
-        if format: value = datetime.strptime(value, format)
-        else: value = datetime.fromtimestamp(int(value))
+        if format:
+            value = datetime.strptime(value, format)
+        else:
+            value = datetime.fromtimestamp(int(value))
     return value
