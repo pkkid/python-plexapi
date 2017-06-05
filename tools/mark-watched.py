56b6ec0e923bf1d703cb665feb59684437b0a270
Michael Shepanski <mjs7231@gmail.com>
2017-06-04 22:26:17 -0400

    Add doc string

--- tools/mark-watched.py
+++ tools/mark-watched.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+This will find every item marked with the collection 'markwatched' and mark
+it watched. Just set run every so often in a scheduled task. This will only
+work for the user you connect to the PlexServer as.
+"""
 from plexapi.server import PlexServer
 
 plex = PlexServer()
