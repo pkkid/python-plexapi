1c37f600fe8665bfa74c7799f7a2b2b259fb4325
Michael Shepanski <mikes@nasuni.com>
2019-06-03 07:48:38 -0400

    mark each episode separatly. Plex freaks out otherwise.

--- tools/plex-markwatched.py
+++ tools/plex-markwatched.py
@@ -28,4 +28,4 @@ if __name__ == '__main__':
                 for episode in item.episodes():
                     if not episode.isWatched:
                         print('%s Marking %s watched.' % (datestr(), episode.title))
-                        item.markWatched()
+                        episode.markWatched()
