1bdba218b97b3ada94dcce74bc02bfa528dd15d4
blacktwin <blacktwin@users.noreply.github.com>
2020-03-18 09:45:52 -0400

    add arts methods to playlist.Playlist Playlists currently do not have or expose any art this is to be a place holder for the possible addition from Plex

--- plexapi/playlist.py
+++ plexapi/playlist.py
@@ -287,3 +287,22 @@ class Playlist(PlexPartialObject, Playable):
     def setPoster(self, poster):
         """ Set . :class:`~plexapi.media.Poster` to :class:`~plexapi.video.Video` """
         poster.select()
+
+    def arts(self):
+        """ Returns list of available art objects. :class:`~plexapi.media.Poster`. """
+
+        return self.fetchItems('/library/metadata/%s/arts' % self.ratingKey)
+
+    def uploadArt(self, url=None, filepath=None):
+        """ Upload art from url or filepath. :class:`~plexapi.media.Poster` to :class:`~plexapi.video.Video`. """
+        if url:
+            key = '/library/metadata/%s/arts?url=%s' % (self.ratingKey, quote_plus(url))
+            self._server.query(key, method=self._server._session.post)
+        elif filepath:
+            key = '/library/metadata/%s/arts?' % self.ratingKey
+            data = open(filepath, 'rb').read()
+            self._server.query(key, method=self._server._session.post, data=data)
+
+    def setArt(self, art):
+        """ Set :class:`~plexapi.media.Poster` to :class:`~plexapi.video.Video` """
+        art.select()
