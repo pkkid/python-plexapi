350f43773aa7d753c0e0333d9c62b4f48e906239
Hellowlol <Hellowlol@users.noreply.github.com>
2020-04-16 01:12:17 +0200

    fix test_settinngs_get

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -8,7 +8,8 @@ def test_settings_get(plex):
     # plex just default to computer name but it NOT in the settings.
     # check this one. why is this bytes instead of string.
     value = plex.settings.get('FriendlyName').value
-    assert value or len(value)
+    # Should not be bytes, fix this when py2 is dropped
+    assert isinstance(value, bytes)
 
 
 def test_settings_set(plex):
