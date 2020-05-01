690abc30088cc1831616ef5343ac4b05fb19d6c6
Hellowlol <Hellowlol@users.noreply.github.com>
2020-05-02 00:17:30 +0300

    Check if gdm is enabled
    on the server before checking for entries

--- tests/test_gdm.py
+++ tests/test_gdm.py
@@ -2,11 +2,13 @@
 from plexapi.gdm import GDM
 
 
-def test_gdm():
+def test_gdm(plex):
     gdm = GDM()
 
+    gdm_enabled = plex.settings.get("GdmEnabled")
+
     gdm.scan(timeout=2)
-    assert len(gdm.entries)
-
-
-
+    if gdm_enabled:
+        assert len(gdm.entries)
+    else:
+        assert not len(gdm.entries)
