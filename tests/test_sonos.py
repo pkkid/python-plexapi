735e26f25752b80b71b50da98581b337b413ffb2
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-05-15 08:45:46 -0500

    Sonos lookup fixes and tests

--- tests/test_sonos.py
+++ tests/test_sonos.py
@@ -8,8 +8,17 @@ def test_sonos_resources(mocked_account, requests_mock):
     speakers = mocked_account.sonos_speakers()
     assert len(speakers) == 3
 
+    # Finds individual speaker by name
     speaker1 = mocked_account.sonos_speaker("Speaker 1")
     assert speaker1.machineIdentifier == "RINCON_12345678901234567:1234567891"
 
+    # Finds speaker as part of group
+    speaker1 = mocked_account.sonos_speaker("Speaker 2")
+    assert speaker1.machineIdentifier == "RINCON_12345678901234567:1234567892"
+
+    # Finds speaker by identifier
     speaker3 = mocked_account.sonos_speaker_by_id("RINCON_12345678901234567:1234567893")
     assert speaker3.title == "Speaker 3"
+
+    assert mocked_account.sonos_speaker("Speaker X") is None
+    assert mocked_account.sonos_speaker_by_id("ID_DOES_NOT_EXIST") is None
