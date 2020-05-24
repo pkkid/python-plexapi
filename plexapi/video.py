a771feac35819a60d40920704b1be4611b0c8ea3
blacktwin <blacktwin@users.noreply.github.com>
2020-05-23 23:06:05 -0400

    import settings

--- plexapi/video.py
+++ plexapi/video.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from plexapi import media, utils
+from plexapi import media, utils, settings
 from plexapi.exceptions import BadRequest, NotFound
 from plexapi.base import Playable, PlexPartialObject
 from plexapi.compat import quote_plus, urlencode
