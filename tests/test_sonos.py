4193012bc2fb99869cf5c9d8fd906274a9bdfab5
jjlawren <jjlawren@users.noreply.github.com>
2020-05-26 15:09:39 -0500

    Improve Sonos ID lookup (#496)

--- tests/test_sonos.py
+++ tests/test_sonos.py
@@ -10,14 +10,18 @@ def test_sonos_resources(mocked_account, requests_mock):
 
     # Finds individual speaker by name
     speaker1 = mocked_account.sonos_speaker("Speaker 1")
-    assert speaker1.machineIdentifier == "RINCON_12345678901234567:1234567891"
+    assert speaker1.machineIdentifier == "RINCON_12345678901234561:1234567891"
 
     # Finds speaker as part of group
     speaker1 = mocked_account.sonos_speaker("Speaker 2")
-    assert speaker1.machineIdentifier == "RINCON_12345678901234567:1234567892"
+    assert speaker1.machineIdentifier == "RINCON_12345678901234562:1234567892"
 
-    # Finds speaker by identifier
-    speaker3 = mocked_account.sonos_speaker_by_id("RINCON_12345678901234567:1234567893")
+    # Finds speaker by Plex identifier
+    speaker3 = mocked_account.sonos_speaker_by_id("RINCON_12345678901234563:1234567893")
+    assert speaker3.title == "Speaker 3"
+
+    # Finds speaker by Sonos identifier
+    speaker3 = mocked_account.sonos_speaker_by_id("RINCON_12345678901234563")
     assert speaker3.title == "Speaker 3"
 
     assert mocked_account.sonos_speaker("Speaker X") is None
