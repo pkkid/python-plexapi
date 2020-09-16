3c726eadeb63c710bdf6cd080e97e6531593b8a8
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-09-16 11:17:40 -0500

    Fix typo

--- tests/test__prepare.py
+++ tests/test__prepare.py
@@ -10,7 +10,7 @@ def wait_for_idle_server(server):
     """Wait for PMS activities to complete with a timeout."""
     attempts = 0
     while server.activities and attempts < MAX_ATTEMPTS:
-        print(f"Watiing for activities to finish: {server.activities}")
+        print(f"Waiting for activities to finish: {server.activities}")
         time.sleep(1)
         attempts += 1
     assert attempts < MAX_ATTEMPTS, f"Server still busy after {MAX_ATTEMPTS}s"
