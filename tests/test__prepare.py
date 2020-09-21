3a3f8f7708482e96e7f290c02f9c874fcc68762b
Jason Lawrence <jjlawren@users.noreply.github.com>
2020-09-20 23:29:19 -0500

    Set tests for normal runs

--- tests/test__prepare.py
+++ tests/test__prepare.py
@@ -27,7 +27,6 @@ def wait_for_metadata_processing(server):
             if tl.updateQueueSize > 0:
                 busy = True
                 print(f"{section.title}: {tl.updateQueueSize} items left")
-                assert not busy, f"{section.title}: {tl.updateQueueSize} items left"
         if not busy or attempts > MAX_ATTEMPTS:
             break
         time.sleep(1)
@@ -35,13 +34,13 @@ def wait_for_metadata_processing(server):
     assert attempts < MAX_ATTEMPTS, f"Metadata still processing after {MAX_ATTEMPTS}s"
 
 
-#def test_ensure_activities_completed(plex):
-#    wait_for_idle_server(plex)
+def test_ensure_activities_completed(plex):
+    wait_for_idle_server(plex)
 
 
-#@pytest.mark.authenticated
-#def test_ensure_activities_completed_authenticated(plex):
-#    wait_for_idle_server(plex)
+@pytest.mark.authenticated
+def test_ensure_activities_completed_authenticated(plex):
+    wait_for_idle_server(plex)
 
 
 def test_ensure_metadata_scans_completed(plex):
