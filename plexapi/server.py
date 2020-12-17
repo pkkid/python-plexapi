37dbec6157ca3d88e1c588b26de6d186426503d1
red4dj <35785346+red4dj@users.noreply.github.com>
2020-12-16 21:25:31 -0500

    Added missing support for /transcode/sessions

--- plexapi/server.py
+++ plexapi/server.py
@@ -545,6 +545,10 @@ class PlexServer(PlexObject):
     def sessions(self):
         """ Returns a list of all active session (currently playing) media objects. """
         return self.fetchItems('/status/sessions')
+      
+    def transcodeSessions(self):
+        """ Returns a list of all active transcode session objects. """
+        return self.fetchItems('/transcode/sessions')
 
     def startAlertListener(self, callback=None):
         """ Creates a websocket connection to the Plex Server to optionally recieve
