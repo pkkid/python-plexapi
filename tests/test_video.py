28a8436dc00f3dff07d25adc2368bfc8857ef07e
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2021-02-04 19:03:29 -0800

    Add test for episode attributes with hidden seasons

--- tests/test_video.py
+++ tests/test_video.py
@@ -739,6 +739,21 @@ def test_video_Episode_history(episode):
     episode.markUnwatched()
 
 
+def test_video_Episode_hidden_season(episode):
+    assert episode.skipParent is False
+    assert episode.parentRatingKey
+    assert episode.parentKey
+    assert episode.seasonNumber
+    show = episode.show()
+    show.editAdvanced(flattenSeasons=1)
+    episode.reload()
+    assert episode.skipParent is True
+    assert episode.parentRatingKey
+    assert episode.parentKey
+    assert episode.seasonNumber
+    show.defaultAdvanced()
+
+
 # Analyze seems to fail intermittently
 @pytest.mark.xfail
 def test_video_Episode_analyze(tvshows):
@@ -765,6 +780,7 @@ def test_video_Episode_attrs(episode):
     assert episode.rating >= 7.7
     assert utils.is_int(episode.ratingKey)
     assert episode._server._baseurl == utils.SERVER_BASEURL
+    assert episode.skipParent is False
     assert utils.is_string(episode.summary, gte=100)
     assert utils.is_metadata(episode.thumb, contains="/thumb/")
     assert episode.title == "Winter Is Coming"
