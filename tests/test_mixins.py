c675858d778df1b1b237b91ecc7a549f2cd06a28
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2021-02-14 14:59:28 -0800

    Clean mixin tag test

--- tests/test_mixins.py
+++ tests/test_mixins.py
@@ -8,20 +8,25 @@ def _test_mixins_tag(obj, attr, tag_method):
     add_tag_method = getattr(obj, "add" + tag_method)
     remove_tag_method = getattr(obj, "remove" + tag_method)
     field_name = tag_singular(attr)
+    _tags = lambda: [t.tag for t in getattr(obj, attr)]
+    _fields = lambda: [f for f in obj.fields if f.name == field_name]
     # Check tag is not present to begin with
-    assert TEST_MIXIN_TAG not in [tag.tag for tag in getattr(obj, attr)]
+    tags = _tags()
+    assert TEST_MIXIN_TAG not in tags
     # Add tag and lock the field
     add_tag_method(TEST_MIXIN_TAG)
     obj.reload()
-    field = [f for f in obj.fields if f.name == field_name]
-    assert TEST_MIXIN_TAG in [tag.tag for tag in getattr(obj, attr)]
-    assert field and field[0].locked
+    tags = _tags()
+    fields = _fields()
+    assert TEST_MIXIN_TAG in tags
+    assert fields and fields[0].locked
     # Remove tag and unlock to field to restore the clean state
     remove_tag_method(TEST_MIXIN_TAG, locked=False)
     obj.reload()
-    field = [f for f in obj.fields if f.name == field_name]
-    assert TEST_MIXIN_TAG not in [tag.tag for tag in getattr(obj, attr)]
-    assert not field
+    tags = _tags()
+    fields = _fields()
+    assert TEST_MIXIN_TAG not in tags
+    assert not fields
 
 
 def edit_collection(obj):
