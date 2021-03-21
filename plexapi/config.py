3434f264fc756c28bfa358fd8f0eebc27817fe12
Elan Ruusamäe <glen@pld-linux.org>
2021-03-21 20:31:16 +0200

    Initialize headers from config in reset_base_headers()

--- plexapi/config.py
+++ plexapi/config.py
@@ -51,16 +51,30 @@ class PlexConfig(ConfigParser):
 
 def reset_base_headers():
     """ Convenience function returns a dict of all base X-Plex-* headers for session requests. """
-    import plexapi
+    from platform import uname
+    from uuid import getnode
+
+    from plexapi import CONFIG, PROJECT, VERSION
+
+    # Plex Header Configuration
+    X_PLEX_PROVIDES = CONFIG.get('header.provides', 'controller')
+    X_PLEX_PLATFORM = CONFIG.get('header.platform', CONFIG.get('header.platorm', uname()[0]))
+    X_PLEX_PLATFORM_VERSION = CONFIG.get('header.platform_version', uname()[2])
+    X_PLEX_PRODUCT = CONFIG.get('header.product', PROJECT)
+    X_PLEX_VERSION = CONFIG.get('header.version', VERSION)
+    X_PLEX_DEVICE = CONFIG.get('header.device', X_PLEX_PLATFORM)
+    X_PLEX_DEVICE_NAME = CONFIG.get('header.device_name', uname()[1])
+    X_PLEX_IDENTIFIER = CONFIG.get('header.identifier', str(hex(getnode())))
+
     return {
-        'X-Plex-Platform': plexapi.X_PLEX_PLATFORM,
-        'X-Plex-Platform-Version': plexapi.X_PLEX_PLATFORM_VERSION,
-        'X-Plex-Provides': plexapi.X_PLEX_PROVIDES,
-        'X-Plex-Product': plexapi.X_PLEX_PRODUCT,
-        'X-Plex-Version': plexapi.X_PLEX_VERSION,
-        'X-Plex-Device': plexapi.X_PLEX_DEVICE,
-        'X-Plex-Device-Name': plexapi.X_PLEX_DEVICE_NAME,
-        'X-Plex-Client-Identifier': plexapi.X_PLEX_IDENTIFIER,
+        'X-Plex-Platform': X_PLEX_PLATFORM,
+        'X-Plex-Platform-Version': X_PLEX_PLATFORM_VERSION,
+        'X-Plex-Provides': X_PLEX_PROVIDES,
+        'X-Plex-Product': X_PLEX_PRODUCT,
+        'X-Plex-Version': X_PLEX_VERSION,
+        'X-Plex-Device': X_PLEX_DEVICE,
+        'X-Plex-Device-Name': X_PLEX_DEVICE_NAME,
+        'X-Plex-Client-Identifier': X_PLEX_IDENTIFIER,
         'X-Plex-Sync-Version': '2',
         'X-Plex-Features': 'external-media',
     }
