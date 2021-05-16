28f0585a971fa9df6aa21460070b3883e3e368c7
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2021-05-15 22:39:26 -0700

    Update doc string for track originalTitle

--- plexapi/audio.py
+++ plexapi/audio.py
@@ -346,7 +346,7 @@ class Track(Audio, Playable, ArtUrlMixin, PosterUrlMixin, CollectionMixin, MoodM
                 (/library/metadata/<grandparentRatingKey>/thumb/<thumbid>).
             grandparentTitle (str): Name of the album artist for the track.
             media (List<:class:`~plexapi.media.Media`>): List of media objects.
-            originalTitle (str): The original title of the track (eg. a different language).
+            originalTitle (str): The artist for the track.
             parentGuid (str): Plex GUID for the album (plex://album/5d07cd8e403c640290f180f9).
             parentIndex (int): Album index.
             parentKey (str): API URL of the album (/library/metadata/<parentRatingKey>).
