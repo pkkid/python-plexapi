e28cfee7e7cfa48ed3da247de614c83296c4cce6
Jason Lawrence <jjlawren@users.noreply.github.com>
2021-07-29 11:22:47 -0500

    Update version handling and add helper script

---
+++ plexapi/const.py
@@ -0,0 +1,9 @@
+"""Constants used by plexapi."""
+from typing import Final
+
+# Library version
+MAJOR_VERSION: Final = 4
+MINOR_VERSION: Final = 7
+PATCH_VERSION: Final = 0
+__short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
+__version__: Final = f"{__short_version__}.{PATCH_VERSION}"
