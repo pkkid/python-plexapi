31cd90184f0ab2276e0f721daad7124deb15a226
jasonl <jjlawren@users.noreply.github.com>
2022-01-13 11:07:03 -0600

    Revert property

--- plexapi/audio.py
+++ plexapi/audio.py
@@ -223,7 +223,6 @@ class Artist(Audio, AdvancedSettingsMixin, ArtMixin, PosterMixin, RatingMixin, S
             filepaths += track.download(_savepath, keep_original_name, **kwargs)
         return filepaths
 
-    @property
     def station(self):
         """ Returns a :class:`~plexapi.playlist.Playlist` artist radio station or `None`. """
         key = '%s?includeStations=1' % self.key
