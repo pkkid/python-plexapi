abdf42098eb81929e1e618adacb94e148559701f
meisnate12 <meisnate12@gmail.com>
2022-01-13 10:56:32 -0500

    fix tests

--- tests/test_video.py
+++ tests/test_video.py
@@ -938,7 +938,7 @@ def test_video_Season_mixins_rating(show):
 def test_video_Season_mixins_tags(show):
     season = show.season(season=1)
     test_mixins.edit_collection(season)
-    test_mixins.edit_label(episode)
+    test_mixins.edit_label(season)
 
 
 def test_video_Season_PlexWebURL(plex, season):
