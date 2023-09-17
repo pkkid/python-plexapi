b129fa37574563cd8426ff392bf5850c2155a8ee
Benjamin Oddou <85166574+BenjaminOddou@users.noreply.github.com>
2023-09-18 01:04:41 +0200

    Update isLatest() (#1253)
    * Update isLatest()
    
    Use the canInstall attribute to determine if the release is the latest build.
    
    * use of utils.cast
    
    Co-authored-by: JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>

--- plexapi/server.py
+++ plexapi/server.py
@@ -630,8 +630,8 @@ class PlexServer(PlexObject):
 
     def isLatest(self):
         """ Check if the installed version of PMS is the latest. """
-        release = self.checkForUpdate(force=True)
-        return release is None
+        release = self.query('/updater/status')
+        return utils.cast(bool, release.get('canInstall'))
 
     def installUpdate(self):
         """ Install the newest version of Plex Media Server. """
