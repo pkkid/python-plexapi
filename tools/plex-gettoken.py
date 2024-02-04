768aa58c908153d1064d05b33d1101deda548ebe
JonnyWong16 <9099342+JonnyWong16@users.noreply.github.com>
2024-02-04 10:49:55 -0800

    Adds 2FA code input to `tools/plex-gettoken.py` (#1319)
    * Use getpass for password input
    
    * Add 2FA code input

--- tools/plex-gettoken.py
+++ tools/plex-gettoken.py
@@ -3,10 +3,12 @@
 """
 Plex-GetToken is a simple method to retrieve a Plex account token.
 """
+from getpass import getpass
 from plexapi.myplex import MyPlexAccount
 
 username = input("Plex username: ")
-password = input("Plex password: ")
+password = getpass("Plex password: ")
+code = input("Plex 2FA code (leave blank for none): ")
 
-account = MyPlexAccount(username, password)
+account = MyPlexAccount(username, password, code=code)
 print(account.authenticationToken)
